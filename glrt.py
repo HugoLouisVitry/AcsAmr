1e864c25405fbdb2109e9a4ec590a5cb8893bfd9
Hugo <hugo.louis.vitry@gmail.com>
2025-02-27 22:04:56 +0100

    pcc instead of pe and image plot

--- glrt.py
+++ glrt.py
@@ -6,8 +6,8 @@ from modulation import *
 
 def apply_fading_channel(signal):
     """ Simuler du fading """
-    alpha = 1# np.random.uniform(0.25,1)
-    theta = 0 # np.pi/3 #np.random.uniform(0,2*np.pi)
+    alpha =  np.random.uniform(0.25,1)
+    theta =  np.random.uniform(0,2*np.pi)
     return signal * alpha * np.exp(-1j * theta), alpha, theta
 
 def add_awgn(signal, snr_db):
@@ -16,15 +16,14 @@ def add_awgn(signal, snr_db):
     power_signal = np.mean(np.abs(signal)**2)
     noise_power = power_signal / snr_linear
     noise = np.sqrt(noise_power/2) * (np.random.randn(*signal.shape) + 1j * np.random.randn(*signal.shape))
-    sigma2 = np.var(noise)
-    return signal + noise, sigma2
+    return signal + noise, noise_power
 
 def blind_channel_estimation(received_signal):
     """ Estimer les paramètres du canal sans connaître le signal transmis """
     return
 
 def Log_Joint_Likelihood(r, alpha, theta, sigma2, symboles_modulation):
-    """ Eq (3.3) 
+    """ Eq (3.3/3.4) 
     \n Calcule la Log Joint likelihood pour des échantillons r et une modulation a tester  
      \n avec les paramètres estimé du canal """
     fading = alpha*np.exp(-1j*theta)
@@ -33,8 +32,10 @@ def Log_Joint_Likelihood(r, alpha, theta, sigma2, symboles_modulation):
     for n in range(len(r)) :
         sum = []
         for Am in symboles_modulation:
-            sum.append( np.exp( -( np.abs(r[n] - fading*Am) ** 2 )/(2*sigma2) ) )
-        log_L.append(np.log(np.sum(sum))+log_cste)
+            t = np.exp( -( np.abs(r[n] - fading*Am) ** 2 )/(2*sigma2) )
+            sum.append(t)
+        logsum= np.log(np.sum(sum))
+        log_L.append((logsum)+log_cste)
     return np.sum(log_L)
 
 def glrt_Likelihood(r, alphas, thetas, sigma2s, symboles_modulation):
@@ -76,9 +77,9 @@ def test_GLRT(Mod_true, N, SNR_dB,alphabet, plots = False):
     received_signal, true_sigma2 = add_awgn(faded_signal, SNR_dB)
 
     # Estimation aveugle des paramètres du canal
-    alpha_est = [true_alpha] #np.random.uniform(0.25,1, size=10)
-    theta_est = [true_theta] #np.random.uniform(0,2*np.pi, size=10)
-    sigma2_est =  [true_sigma2] #np.random.uniform(0.1,2, size=10)
+    alpha_est  = [1]                    # np.linspace(0.25,1, 20) # [true_alpha for _ in range(20)] # 
+    theta_est  = [0]           # for _ in range(20)]  # np.linspace(0,2*np.pi,20) #[true_theta for _ in range(100)] #
+    sigma2_est = [true_sigma2]          # for _ in range(20)] # np.arange(0.001,1000, 1000)
 
     M_estimated = glrt_classification(received_signal, alphabet, alpha_est, theta_est, sigma2_est)
 
@@ -87,7 +88,7 @@ def test_GLRT(Mod_true, N, SNR_dB,alphabet, plots = False):
         plt.figure(figsize=(8, 8))
 
         plt.scatter(received_signal.real, received_signal.imag, color='blue', alpha=0.5, label=f'Symboles reçus\n Estimé: {M_estimated}')
-        plt.scatter(signal.real,signal.imag,color='red', marker='x', label=f'Symboles idéaux {Mod_true}')
+        plt.scatter(signal.real,signal.imag,color='red', marker='x', label=f'Symboles idéaux {Mod_true} \nalpha:{true_alpha}\ntheta:{true_theta}\nsigma2:{true_sigma2}')
         #plt.scatter(qam_Alphabet["64QAM"].real,qam_Alphabet["64QAM"].imag,color='green', marker='o',alpha=0.8, label=f'Alphabet')
 
         plt.grid(True, linestyle='--', alpha=0.6)
@@ -107,41 +108,44 @@ def test_GLRT(Mod_true, N, SNR_dB,alphabet, plots = False):
         return False
 
 def taux_erreur(Ms_true,  SNRs_dB, N_echantillons, N_test, alphabet):
-    plt.figure()
+    plt.figure(dpi=600)
     total = time()
+    print("Started")
     for m_test in Ms_true :
         t = time() 
-        Tes = []
+        PCC = []
         for SNR_dB in SNRs_dB :
-            Te = 0
+            Pc = 0
             for _ in range(N_test):
+                # t2 = time()
                 correct_classification = test_GLRT(m_test, N_echantillons, SNR_dB,alphabet)
-                if not correct_classification :
-                    Te = Te + 1
-            Tes.append(Te / N_test)
+                if correct_classification :
+                    Pc = Pc + 1
+                # print(f'{_} :{time()-t2} s')
+            PCC.append(Pc / N_test * 100)
         print(f'{m_test} :{time()-t} s')
         if m_test.find("QAM") != -1 :
             Linestyle = ':'
         else : 
             Linestyle = '-'
-        plt.plot(SNRs_dB, Tes, marker='o', linestyle=Linestyle , label=f"{m_test}")
+        plt.plot(SNRs_dB, PCC, marker='o', linestyle=Linestyle , label=f"{m_test}")
     
     
     plt.xlabel("SNR (dB)")
-    plt.ylabel("Taux d'erreur de classification")
-    plt.title(f"Taux d'erreur pour {Ne} echantillons transmis")
+    plt.ylabel("Pcc %")
+    plt.title(f"Taux de réussite de classification (Pcc) en fonction du SNR (unknown fading)")
     plt.legend()
     plt.grid()
     print(f'Total execution time : {(time()-total)/60} min')
-    return Tes
+    return PCC
 
 
+if __name__ == "__main__":
+    M_true = "16QAM"  
+    Ne = 500  # Nombre d'échantillon
+    SNR_dB = 16
+    #a = test_GLRT(M_true, Ne, SNR_dB,ALPHABET, True)
+    # print(a)
 
-M_true = "8PSK"  
-Ne = 30  # Nombre d'échantillon
-SNR_dB = 16
-a = test_GLRT(M_true, Ne, SNR_dB,ALPHABET, True)
-print(a)
-
-taux_erreur(MODLIST, [i for i in range(4,21,1)], Ne, 50, ALPHABET)
-plt.show()
+    taux_erreur(MODLIST, [i for i in range(-20,21,2)], Ne, 100, ALPHABET)
+    plt.show()
